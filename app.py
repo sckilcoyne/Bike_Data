66384833f9de6a41720bb3a2799402e75fafa35b
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2022-10-09 21:14:35 -0400

    add some data displays

--- app.py
+++ app.py
@@ -260,4 +260,7 @@ with tabOther:
 
     ### Bluebikes data explorations:    
     [BlueBikes volume per street segment for July 2022](https://crschmidt.net/bluebikes/july2022) by [@crschmidt](https://twitter.com/crschmidt/status/1567888389281665024?s=20&t=1bSy86Pw7Q66Fq9NCTOgNg)
+    https://datastudio.google.com/u/0/reporting/650b5e38-07a0-4c95-8f16-f0f9f22b7d98/page/tEnnC
+    
+    https://www.bostonindicators.org/reports/report-website-pages/covid_indicators-x2/2021/february/biking-brief
     '''
