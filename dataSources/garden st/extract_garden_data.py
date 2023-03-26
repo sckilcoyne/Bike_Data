93bda88de0110f9fbd371f374c476dcfee6d39ce
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2023-03-25 21:51:53 -0400

    progress

--- dataSources/garden st/extract_garden_data.py
+++ dataSources/garden st/extract_garden_data.py
@@ -10,20 +10,20 @@ import pandas as pd
 
 # %% Files
 
-folder = 'C:/Users/Scott/Documents/GitHub/Bike_Data/data/garden st/'
+folder = 'C:/Users/Scott/Documents/GitHub/Bike_Data/dataSources/garden st/'
 ext = '.pdf'
 
 pdf_file = f'{folder}gardenstreetativydecembercounts{ext}'
 
-pdf_2022 = {'garden101222atrcounts': {
-    'NB': [1, 3],
-    'SB': [2, 4]},
+pdf1 = {'garden101222atrcounts': {
+    'NB': [1],
+    'SB': [2]},
     'gardenstreetativydecembercounts': {
     'NB': [1, 3],
     'SB': [2, 4]},
 }
 
-pdf_2023 = {'gardenivy2023jan1112': {
+pdf2 = {'gardenivy2023jan1112': {
     'direction1': [1, 3],
     'direction2': [2, 4]},
     'gardenivy2023feb1516': {
@@ -87,12 +87,61 @@ def format1(filename, direction, pageNumbers):
     return df
 
 
-df2022 = pd.DataFrame()
-for pdf in pdf_2022:
-    # print(pdf)
-    for direction in pdf_2022[pdf]:
-        # print(direction)
-        # print(pdf_2022[pdf][direction])
+def format2(filename, direction, pageNumbers):
+    # Get the number of pages in the file
+    # pdf_reader = pypdf.PdfReader(filename)
+    # n_pages = len(pdf_reader.pages)
+
+    # For each page the table can be read with the following code
+    table_pdf = read_pdf(
+        filename,
+        guess=True,
+        pages=pageNumbers,
+        stream=True,
+        encoding="utf-8",
+    )
+
+    df = pd.DataFrame()
+    headers = ['Time', 'Bicycles', 'Motorcycle', 'Cars & lights Goods',
+               'Buses', 'Single Heavy Unit', 'Multi Heavy Unit', 'Total']
+    print(f'{pageNumbers=}')
+    for i, _ in enumerate(pageNumbers):
+        print(f'{i=}')
+        dfTable = table_pdf[i]
+
+        # Remove top rows with annoying headers
+        dfTable = dfTable.iloc[4:]
+
+        # Remove separating column
+        dfTable = dfTable.drop(
+            ['Unnamed: 3', 'Unnamed: 4', 'Unnamed: 9', 'Unnamed: 13'], axis=1)
+
+        # Move PM table below AM table
+        dfAM = dfTable.iloc[:, :8]
+        dfPM = dfTable.iloc[:, 8:]
+        dfTable = pd.concat([dfAM, dfPM], ignore_index=True)
+
+        # Fix headers
+        dfTable.columns = headers
+
+        print('pause')
+
+# %% Run Script
+
+# df1 = pd.DataFrame()
+# for pdf in pdf1:
+#     # print(pdf)
+#     for direction in pdf1[pdf]:
+#         # print(direction)
+#         # print(pdf_2022[pdf][direction])
+#         pdf_file = f'{folder}{pdf}{ext}'
+#         dfAdd = format1(pdf_file, direction, pdf1[pdf][direction])
+#         df1 = pd.concat([df1, dfAdd])
+
+
+df2 = pd.DataFrame()
+for pdf in pdf2:
+    for direction in pdf2[pdf]:
         pdf_file = f'{folder}{pdf}{ext}'
-        dfAdd = format1(pdf_file, direction, pdf_2022[pdf][direction])
-        df2022 = pd.concat([df2022, dfAdd])
+        dfAdd = format2(pdf_file, direction, pdf2[pdf][direction])
+        df2 = pd.concat([df2, dfAdd])
