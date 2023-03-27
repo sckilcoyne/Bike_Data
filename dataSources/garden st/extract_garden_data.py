fd5a30f7e422f2a91f2d79805f31e626ecc4a7ee
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2023-03-26 23:29:04 -0400

    format1 ready for reading whole folder

--- dataSources/garden st/extract_garden_data.py
+++ dataSources/garden st/extract_garden_data.py
@@ -7,52 +7,47 @@ import pypdf
 from tabula import read_pdf
 import numpy as np
 import pandas as pd
+import glob
 
 # %% Files
 
 folder = 'C:/Users/Scott/Documents/GitHub/Bike_Data/dataSources/garden st/'
 ext = '.pdf'
 
-pdf_file = f'{folder}gardenstreetativydecembercounts{ext}'
-
-pdf1 = {'garden101222atrcounts': {
-    'NB': [1],
-    'SB': [2]},
-    'gardenstreetativydecembercounts': {
-    'NB': [1, 3],
-    'SB': [2, 4]},
-}
-
-pdf2 = {'gardenivy2023jan1112': {
-    'direction1': [1, 3],
-    'direction2': [2, 4]},
-    'gardenivy2023feb1516': {
-    'direction1': [1, 3],
-    'direction2': [2, 4]},
-}
+commonHeaders = ['Datetime', 'Location', 'Direction',
+                 'Bicycles', 'Cars', 'Total']
 
 # %% Open Original Data
 
 
-def format1(filename, direction, pageNumbers):
+def format1(filename, metaData):
     # Get the number of pages in the file
     # pdf_reader = pypdf.PdfReader(filename)
     # n_pages = len(pdf_reader.pages)
 
-    # For each page the table can be read with the following code
-    table_pdf = read_pdf(
-        filename,
-        guess=True,
-        pages=pageNumbers,
-        stream=True,
-        encoding="utf-8",
-    )
+    location = metaData[0]
+    print(f'{location=}')
+    dates = metaData[1]
+    print(f'{dates=}')
 
     df = pd.DataFrame()
-    print(f'{pageNumbers=}')
-    for i, _ in enumerate(pageNumbers):
-        print(f'{i=}')
-        dfTable = table_pdf[i]
+
+    for page in metaData[3:]:
+        print(f'{page=}')
+        pageInfo = page.split('_')
+        direction = pageInfo[0]
+        pageNum = pageInfo[1]
+
+        # For each page the table can be read with the following code
+        table_pdf = read_pdf(
+            f'{folder}{filename}{ext}',
+            guess=True,
+            pages=pageNum,
+            stream=True,
+            encoding="utf-8",
+        )
+
+        dfTable = table_pdf[0]
         # Remove bottom rows with summary info
         dropSummary = dfTable.iloc[:, 0].index[dfTable.iloc[:, 0] == 'Total']
         dfTable = dfTable.iloc[0:dropSummary[0], :]
@@ -73,7 +68,8 @@ def format1(filename, direction, pageNumbers):
         dfTable = dfTable.rename(columns={timeCol: 'Time'})
 
         # Remove unnamed from columns
-        dfTable.columns = dfTable.columns.str.replace(r'Unnamed: [0-9]* ', '')
+        dfTable.columns = dfTable.columns.str.replace(
+            r'Unnamed: [0-9]* ', '', regex=True)
 
         # Add Date Column
         dfTable['Date'] = dateData
@@ -82,32 +78,52 @@ def format1(filename, direction, pageNumbers):
         dfTable['Datetime'] = pd.date_range(
             start=dateData, periods=24, freq='H')
 
-    df = pd.concat([df, dfTable])
+        # Add direction and location
+        dfTable['Direction'] = direction
+        dfTable['Location'] = location
 
-    return df
+        df = pd.concat([df, dfTable])
+
+    # Create a simple dataframe with common headers between report types
+    dfSimple = df[['Datetime', 'Location', 'Direction',
+                   'Bicycles', 'Cars & Trailers', 'Total']].copy()
+    dfSimple.columns = commonHeaders
+
+    return dfSimple
 
 
-def format2(filename, direction, pageNumbers):
+def format2(filename, metaData):
     # Get the number of pages in the file
     # pdf_reader = pypdf.PdfReader(filename)
     # n_pages = len(pdf_reader.pages)
 
-    # For each page the table can be read with the following code
-    table_pdf = read_pdf(
-        filename,
-        guess=True,
-        pages=pageNumbers,
-        stream=True,
-        encoding="utf-8",
-    )
+    location = metaData[0]
+    print(f'{location=}')
+    dates = metaData[1].split('_')
+    print(f'{dates=}')
+
+    # pageCount = len(metaData[3:])
 
     df = pd.DataFrame()
-    headers = ['Time', 'Bicycles', 'Motorcycle', 'Cars & lights Goods',
+    headers = ['Time', 'Bicycles', 'Motorcycle', 'Cars & light Goods',
                'Buses', 'Single Heavy Unit', 'Multi Heavy Unit', 'Total']
-    print(f'{pageNumbers=}')
-    for i, _ in enumerate(pageNumbers):
-        print(f'{i=}')
-        dfTable = table_pdf[i]
+
+    for page in metaData[3:]:
+        print(f'{page=}')
+        pageInfo = page.split('_')
+        direction = pageInfo[0]
+        pageNum = pageInfo[1]
+
+        # For each page the table can be read with the following code
+        table_pdf = read_pdf(
+            f'{folder}{filename}{ext}',
+            guess=True,
+            pages=pageNum,
+            stream=True,
+            encoding="utf-8",
+        )
+
+        dfTable = table_pdf[0]
 
         # Remove top rows with annoying headers
         dfTable = dfTable.iloc[4:]
@@ -119,12 +135,21 @@ def format2(filename, direction, pageNumbers):
         # Move PM table below AM table
         dfAM = dfTable.iloc[:, :8]
         dfPM = dfTable.iloc[:, 8:]
+        dfAM.columns = headers
+        dfPM.columns = headers
         dfTable = pd.concat([dfAM, dfPM], ignore_index=True)
 
         # Fix headers
         dfTable.columns = headers
 
+        # Create date then datetime columns
+        if len(dates) == 1:
+            dfTable['Date'] = dates[0]
+        elif len(dates) > 1:
+
         print('pause')
+
+    return df
 
 # %% Run Script
 
@@ -139,9 +164,26 @@ def format2(filename, direction, pageNumbers):
 #         df1 = pd.concat([df1, dfAdd])
 
 
-df2 = pd.DataFrame()
-for pdf in pdf2:
-    for direction in pdf2[pdf]:
-        pdf_file = f'{folder}{pdf}{ext}'
-        dfAdd = format2(pdf_file, direction, pdf2[pdf][direction])
-        df2 = pd.concat([df2, dfAdd])
+# df2 = pd.DataFrame()
+# for pdf in pdf2:
+#     for direction in pdf2[pdf]:
+#         pdf_file = f'{folder}{pdf}{ext}'
+#         dfAdd = format2(pdf_file, direction, pdf2[pdf][direction])
+#         df2 = pd.concat([df2, dfAdd])
+
+def main():
+    pdfList = glob.glob(f'{folder}*{ext}')
+    pdfList = [x[len(folder):-len(ext)] for x in pdfList]
+
+    dfAll = pd.DataFrame()
+    for pdf in pdfList:
+        metaData = pdf.split('-')
+        if metaData[2] == 'T1':
+            dfAll = pd.concat([dfAll, format1(pdf, metaData)])
+        elif metaData[2] == 'T2':
+            dfAll = pd.concat([dfAll, format2(pdf, metaData)])
+
+    return dfAll
+
+
+df = main()
