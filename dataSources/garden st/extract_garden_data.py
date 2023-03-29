2085924a4636081ebe9b8c0c26acdd49d2e3f1fd
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2023-03-29 00:24:20 -0400

    data and plots ready for release

--- dataSources/garden st/extract_garden_data.py
+++ dataSources/garden st/extract_garden_data.py
@@ -1,13 +1,19 @@
 '''
+https://www.cambridgema.gov/streetsandtransportation/projectsandprograms/gardenstsafetyimprovementproject#9fe1189863574e38bc5fba895244666c
 
 https://stackoverflow.com/questions/56017702/how-to-extract-table-from-pdf-in-python
 '''
 # %% Initialize
-import pypdf
+# import pypdf
 from tabula import read_pdf
 import numpy as np
 import pandas as pd
 import glob
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from scipy import stats
+import matplotlib.dates as mdates
+import matplotlib.ticker as mtick
 
 # %% Files
 
@@ -121,6 +127,7 @@ def format2(filename, metaData):
             pages=pageNum,
             stream=True,
             encoding="utf-8",
+            silent=True,
         )
 
         dfTable = table_pdf[0]
@@ -154,7 +161,7 @@ def format2(filename, metaData):
 
         # Add Datetime column
         dfTable['Datetime'] = pd.to_datetime(
-            dfTable['Date'] + ' ' + dfTable['Time'])
+            dfTable['Date'] + ' ' + dfTable['Time'], format='%y%m%d %I:%M %p')
 
         # Add direction and location
         dfTable['Direction'] = direction
@@ -169,10 +176,8 @@ def format2(filename, metaData):
 
     return dfSimple
 
-# %% Run Script
 
-
-def main():
+def collect_data():
     pdfList = glob.glob(f'{folder}*{ext}')
     pdfList = [x[len(folder):-len(ext)] for x in pdfList]
 
@@ -184,7 +189,117 @@ def main():
         elif metaData[2] == 'T2':
             dfAll = pd.concat([dfAll, format2(pdf, metaData)])
 
+    # Remove rows without real data
+    dfAll = dfAll.replace('*', np.NaN)
+    dfAll = dfAll.dropna(axis=0, how='any')
+
+    # Set column types
+    dfAll[['Bicycles', 'Cars', 'Total']] = dfAll[[
+        'Bicycles', 'Cars', 'Total']].apply(pd.to_numeric)
+
     return dfAll
 
 
-df = main()
+# %% Run Script
+
+
+df = collect_data()
+
+df.to_csv('all_garden_data.csv')
+
+# %% Import Data
+dfBluebikes = pd.read_csv('bluebikes.txt', header=0)
+dfBluebikes['Bike_Growth'] = 100 * \
+    dfBluebikes['Rides'] / dfBluebikes['Rides'][0]
+dfBluebikes['Date'] = pd.to_datetime(dfBluebikes['Date'])
+
+# %% Summary Data
+
+dfDate = df.groupby(['Location', df.Datetime.dt.date])[[
+    'Bicycles', 'Cars', 'Total']].sum()
+dfDate = dfDate.reset_index(level=1)
+
+dfDate['Bike_Car_Share'] = dfDate['Bicycles'] / dfDate['Cars'] * 100
+dfDate['Bike_Total_Share'] = dfDate['Bicycles'] / dfDate['Cars'] * 100
+# dfDate['Bike_Percentile'] = dfDate['Bicycles'] / dfDate['Bicycles'].min()
+
+dfDate = dfDate.sort_values(by='Datetime')
+
+
+def norm2first(df):
+    # firstdate = df.Datetime[0]
+    # firstDateBike = df.Bicycles[0]
+
+    df['Bike_Growth'] = df['Bicycles'] / df['Bicycles'][0] * 100
+
+    return df
+
+
+dfDate = dfDate.groupby(['Location']).apply(norm2first)
+
+dateMin = mdates.date2num(dfDate.Datetime.min())
+dateMax = mdates.date2num(dfDate.Datetime.max())
+
+# Plot mode share over time
+fig = plt.figure(figsize=(15, 9), dpi=600)
+ax = fig.add_subplot(1, 1, 1)
+ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+x = mdates.date2num(dfDate['Datetime'])
+m, b = np.polyfit(x, dfDate['Bike_Car_Share'], 1)
+plt.plot(x, m*x+b)
+
+for loc in dfDate.groupby('Location'):
+    plt.scatter(loc[1]['Datetime'], loc[1]
+                ['Bike_Car_Share'], label=loc[0], marker='.')
+
+sharePoint = (m*dateMax+b) - (m*dateMin+b)
+sharePercentage = 100 * (m*dateMax+b) / (m*dateMin+b)
+print(f'Bike Mode Share percentage point increase: {sharePoint:0.1f}')
+print(f'Bike Mode Share percentage increase: {sharePercentage:0.0f}%')
+ax.text(
+    0.5, 0.9, f'Bike mode share increased by {sharePercentage:0.0f}%\nand {sharePoint:0.1f} percentage points',
+    transform=ax.transAxes, ha='center')
+
+plt.legend()
+plt.xlabel('Count Date')
+plt.ylabel('Bike Mode Share')
+plt.suptitle(
+    'Bike/Car Mode Share on streets near Garden Street Safety Improvement Project')
+plt.title('Garden St. project implemented in Novemeber 2022')
+ax.text(0.1, 0.01, '@BostonBikeData', transform=ax.transAxes)
+plt.show()
+fig.savefig('bike_modeshare.png')
+
+
+# Plot bike growth over time
+fig = plt.figure(figsize=(15, 9), dpi=600)
+ax = fig.add_subplot(1, 1, 1)
+ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+x = mdates.date2num(dfDate['Datetime'])
+m, b = np.polyfit(x, dfDate['Bike_Growth'], 1)
+
+plt.plot(dfBluebikes['Date'], dfBluebikes['Bike_Growth'],
+         label='Cambridge BlueBikes Rides')
+
+for loc in dfDate.groupby('Location'):
+    plt.scatter(loc[1]['Datetime'], loc[1]
+                ['Bike_Growth'], label=loc[0], marker='.')
+
+growthPercent = (m*dateMax+b) - (m*dateMin+b)
+print(f'Bike volume growth: {growthPercent:.0f}%')
+ax.text(
+    0.5, 0.9, f'Bike volume increased by {growthPercent:.0f}%',
+    transform=ax.transAxes, ha='center')
+
+plt.plot(x, m*x+b)
+plt.legend()
+plt.xlabel('Count Date')
+plt.ylabel('Bike Growth')
+plt.suptitle(
+    'Bike volume relative to first count on streets near Garden Street Safety Improvement Project')
+plt.title('Garden St. project implemented in Novemeber 2022')
+ax.text(0.1, 0.01, '@BostonBikeData', transform=ax.transAxes)
+plt.show()
+fig.savefig('bike_growth.png')
