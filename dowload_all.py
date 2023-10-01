92a7379327fcfaf11183a45c4f11db85dbd3d0f4
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2023-09-30 20:51:08 -0400

    add new counters

--- dowload_all.py
+++ dowload_all.py
@@ -11,48 +11,49 @@ import utils.data_analysis as da
 
 cols_standard = ['StationID', 'StationName', 'Mode', 'DateTime', 'Count']
 
-countersNMDS = json.load(open("dataSources/nmds_test.json", encoding="utf8"))
-countersCODP = json.load(open("dataSources/codp_counters.json", encoding="utf8"))
+countersNMDS = json.load(open("settings/counters_nmds.json", encoding="utf8"))
+countersCODP = json.load(open("settings/counters_codp.json", encoding="utf8"))
 
 # %% Download Data - NMDS
 
-# for c in countersNMDS:
-c = countersNMDS[0]
+for c in countersNMDS[8:9]:
+# c = countersNMDS[0]
 
-# Load full and daily datasets of counter, create empty df if it doesn't exsist
-filename_full = f'data/{c[0]}_full.pkl'
-filename_daily = f'data/{c[0]}_daily.pkl'
+    print(c)
+    # Load full and daily datasets of counter, create empty df if it doesn't exsist
+    filename_full = f'data/{c[0]}_full.pkl'
+    filename_daily = f'data/{c[0]}_daily.pkl'
 
-cdf_full = pd.DataFrame(columns=cols_standard)
-cdf_full = cdf_full.astype({'DateTime': 'datetime64'})
+    cdf_full = pd.DataFrame(columns=cols_standard)
+    cdf_full = cdf_full.astype({'DateTime': 'datetime64'})
 
-cdf_daily = pd.DataFrame(columns=cols_standard)
-cdf_daily = cdf_daily.astype({'DateTime': 'datetime64'})
+    cdf_daily = pd.DataFrame(columns=cols_standard)
+    cdf_daily = cdf_daily.astype({'DateTime': 'datetime64'})
 
-# Create numpy array of dates with data
-datadates = nmds.get_dates(c[0])
+    # Create numpy array of dates with data
+    datadates = nmds.get_dates(c[0])
 
-datelist = set(sorted(set(datadates))[:-3])
-datelist = set([datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in datelist])
+    datelist = set(sorted(set(datadates))[:-3])
+    datelist = set([datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in datelist])
 
-# Download data from missing dates
-newcdf_full = nmds.main(c, datelist)
-if newcdf_full is None:
-    print('No new data downloaded from NMDS')
-else:
-    # Update the daily count dataframe
-    newcdf_daily = da.daily_counts(newcdf_full)
-    print('Calculated daily counts for new data')
+    # Download data from missing dates
+    newcdf_full = nmds.main(c, datelist)
+    if newcdf_full is None:
+        print('No new data downloaded from NMDS')
+    else:
+        # Update the daily count dataframe
+        newcdf_daily = da.daily_counts(newcdf_full)
+        print('Calculated daily counts for new data')
 
-    # Add new data to existing data
-    cdf_full = pd.concat([cdf_full, newcdf_full], ignore_index=True)
-    cdf_daily = pd.concat([cdf_daily, newcdf_daily], ignore_index=True)
-    print('Appened new data to existing data')
+        # Add new data to existing data
+        cdf_full = pd.concat([cdf_full, newcdf_full], ignore_index=True)
+        cdf_daily = pd.concat([cdf_daily, newcdf_daily], ignore_index=True)
+        print('Appened new data to existing data')
 
-    # Save data to file
-    cdf_full.to_pickle(filename_full, protocol=3)
-    cdf_daily.to_pickle(filename_daily, protocol=3)
-    print('Saved data files')
+        # Save data to file
+        cdf_full.to_pickle(filename_full, protocol=3)
+        cdf_daily.to_pickle(filename_daily, protocol=3)
+        print('Saved data files')
 
 # %% Download Data - CODP
 # Can only download 1000 entries at a time using API, just download csv from site instead
